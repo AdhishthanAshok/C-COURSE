d4a6f3e45cd9d15cfd8c003200a5d5aa963c4c7b
Adhishthan Ashok <96656037+AdhishthanAshok@users.noreply.github.com>
2022-08-22 11:51:08 +0530

    Add files via upload

---
+++ tut3.cpp
@@ -0,0 +1,26 @@
+# include <iostream>
+using namespace std;
+
+int c = 10;
+
+int main(){
+    // int a = 12 , b = 6 , c;
+    // cout<<"Enter the value of a"<<endl;
+    // cin>>a;
+    // cout<<"Enter the value of b"<<endl;
+    // cin>>b;
+    // c = a + b;
+    // cout<<"The sum is: "<<c<<endl;
+    // cout<<"The sum is: "<<(::c + c);
+    
+    float d = 65.6;
+    
+    cout<<"The size of 65.6 is : " <<sizeof (65.6)<<endl;
+    cout<<"The size of 65.6f is : "<<sizeof (65.6f)<<endl;
+    cout<<"The size of 65.6F is : "<<sizeof (65.6F)<<endl;
+    cout<<"The size of 65.6l is : "<<sizeof (65.6l)<<endl;
+    cout<<"The size of 65.6L is : "<<sizeof (65.6L)<<endl;
+
+
+    return 0;
+}
---
+++ tut4.cpp
@@ -0,0 +1,16 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+//     const int a = 66;
+//     cout<<"value is: "<<a<<endl;
+//     // a = 56;
+//     cout<<"value is: "<<a<<endl;
+    // int i=0;
+    int i;
+    if(i=0;i<4;i++){
+        cout<<"Value of i is: "<<i<<endl;
+    
+        }
+    return 0;
+}
