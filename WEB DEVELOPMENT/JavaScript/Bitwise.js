9414d0f5714d3306e4b9862c6e78ff6104406eb8
Adhishthan <adhishthanashok@gmail.com>
2023-09-18 23:04:15 +0530

    Bitwise Operators using JS

---
+++ WEB DEVELOPMENT/JavaScript/Bitwise.js
@@ -0,0 +1,20 @@
+let a = 6;
+let b = 1;
+
+console.log("\nAND Operation");
+console.log("A & B = " + (a & b));
+
+console.log(" \nOR operation");
+console.log("A | B = " + (a | b));
+
+console.log(" \nNOT operation");
+console.log("~A = " + ~a);
+
+console.log("\nSign Propagating Right Shift");
+console.log("A >> B = " + (a >> b));
+
+console.log("\nZero Fill Right Shift");
+console.log("A >>> B = " + (a >>> b));
+
+console.log("\nLeft Shift");
+console.log("A << B = " + (a << b));
