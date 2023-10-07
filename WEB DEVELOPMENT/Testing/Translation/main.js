844b259705189cf72b816b05ce36c77039ed6036
Adhishthan <adhishthanashok@gmail.com>
2023-10-07 23:15:24 +0530

    Finished the Live Language Translation WebApp

--- WEB DEVELOPMENT/Testing/Translation/main.js
+++ WEB DEVELOPMENT/Testing/Translation/main.js
@@ -1,9 +1,8 @@
-
 let langOption = document.querySelectorAll("select");
 let fromText = document.querySelector(".fromText");
 let transText = document.querySelector(".toTranslate");
-let fromVoice = document.querySelector(".from");
-let toVoice = document.querySelector(".to");
+let fromVoice = document.getElementById("fromSpeech");
+let toVoice = document.getElementById("toSpeech");
 
 // making the option  value interact with the API
 langOption.forEach((get, con) => {
@@ -12,7 +11,7 @@ langOption.forEach((get, con) => {
     if (con == 0 && countryCode == "en-GB") {
       selected = "selected";
     } else if (con == 1 && countryCode == "hi-IN") {
-      selected = "selected";
+      selected = "selected";  
     }
     let option = `<option value="${countryCode}"${selected}>${language[countryCode]}</option>`;
     get.insertAdjacentHTML("beforeend", option);
@@ -34,24 +33,36 @@ fromText.addEventListener("input", function () {
     });
 });
 
-// making the text to voice feature for the Input
 fromVoice.addEventListener("click", function () {
   let fromTalk;
   fromTalk = new SpeechSynthesisUtterance(fromText.value);
   fromTalk.lang = langOption[0].value;
   console.log('Attempting to speak:', fromTalk);
-  speechSynthesis.speak(fromTalk);
-  console.log("second icon clicked");
+  window.speechSynthesis.speak(fromTalk);
+  console.log("first icon clicked");
 });
 
-// making the text to voice feature for the output
 toVoice.addEventListener("click", function () {
-  let fromTalk;
-  fromTalk = new SpeechSynthesisUtterance(transText.value);
-  fromTalk.lang = langOption[1].value;
-  console.log('Attempting to speak:', fromTalk);
-  speechSynthesis.speak(fromTalk);
-  console.log("second icon clicked");
-  console.log('Selected language code:', langOption[1].value);
-
+  let toTalk;
+  toTalk = new SpeechSynthesisUtterance(transText.value);
+  toTalk.lang = langOption[1].value;
+  window.speechSynthesis.speak(toTalk);
 });
+
+
+
+// let speech = new SpeechSynthesisUtterance();
+
+// document.getElementById("fromSpeech").addEventListener("click" , () =>{
+//     speech.text = document.getElementsByClassName("fromText").value;
+//     window.speechSynthesis.speak(speech);
+// })
+// document.getElementById("toSpeech").addEventListener("click" , () =>{
+//     speech.text = document.getElementsByClassName("toTranslate").value;
+//     window.speechSynthesis.speak(speech);
+// })
+
+
+
+
+
