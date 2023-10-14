8d0d3b40a4dbdda6f7121fa66c37e29659b0f658
Adhishthan <adhishthanashok@gmail.com>
2023-10-14 15:41:48 +0530

    Added React Tutorial File

---
+++ WEB DEVELOPMENT/React/react-task-tracker/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Components/Header";
+
+const App = () => {
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  );
+};
+
+// class App extends React.Component {
+//   render() {
+//     return <h1>Hello from a Class</h1>;
+//   }
+// }
+export default App;
