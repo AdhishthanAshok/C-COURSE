15c30451ff0d65e850415ccd7fef0f1b53b56998
Adhishthan <adhishthanashok@gmail.com>
2023-11-10 21:54:50 +0530

    Removed unnecessary files and made my folder clean

--- WEB DEVELOPMENT/JavaScript/1st.js
+++
@@ -1,7 +0,0 @@
-let newdate = new Date();
-
-console.log(newdate.getDate());
-console.log(newdate.getTime());
-console.log(newdate.getFullYear());
-console.log(newdate.getDay());
-console.log(newdate.getHours());
