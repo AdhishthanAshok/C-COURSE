538fdeeeae73143880393ea0d2f3cc6164aff8f7
Adhishthan <adhishthanashok@gmail.com>
2023-11-11 18:54:06 +0530

    Node JS learning

---
+++ WEB DEVELOPMENT/MERN/Node.js/modulefirst.js
@@ -0,0 +1,4 @@
+import simple from "./modulesecond.mjs";
+// import {simple , simple2 } from "./modulesecond.mjs";
+simple();
+// simple2();
---
+++ WEB DEVELOPMENT/MERN/Node.js/modulesecond.mjs
@@ -0,0 +1,6 @@
+export default function simple() {
+  console.log("Hello World");
+}
+// export default function simple2() {
+//   console.log("Hello World --> 2");
+// }
