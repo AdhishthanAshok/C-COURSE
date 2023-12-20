35d365704ae4858e805d95cf6eb8a88e16743663
Adhishthan <adhishthanashok@gmail.com>
2023-12-20 17:39:41 +0530

    Deleting Python Files

--- Python/copy.py
+++
@@ -1,9 +0,0 @@
-from shutil import copyfile
-from sys import exit 
-source = input("Enter source file with full path: ")
-target = input("Enter target file with full path: ")
-try: 
-     copyfile(source , target) 
-     print("Content of file xyz.txt is copied to file abc.txt")
-except IOError as e:
-    print("Unable to copy file. %s" %e)
