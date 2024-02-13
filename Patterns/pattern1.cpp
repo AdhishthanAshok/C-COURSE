8544c879bb532142343f8f34577abb079dfe2d4d
Adhishthan <adhishthanashok@gmail.com>
2024-02-14 00:32:10 +0530

    Added Patters

--- Patterns/pattern1.cpp
+++ Patterns/pattern1.cpp
@@ -3,6 +3,17 @@ using namespace std;
 
 int main()
 {
-
+    int n;
+    cout << "Enter the patter rows : ";
+    cin >> n;
+    cout << endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
     return 0;
 }
