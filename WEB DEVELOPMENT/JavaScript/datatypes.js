80c7ad24dde27436c35ac018444f21b158419c57
Adhishthan <adhishthanashok@gmail.com>
2024-03-05 23:24:26 +0530

    Code aaur Chai JS

---
+++ WEB DEVELOPMENT/JavaScript/datatypes.js
@@ -0,0 +1,5 @@
+"use strict"; // treat all js code as newer version --> Nowadays it is inderstood so no need to write this .. but it is a good practice ..
+
+// alert(3+3)   --> can not be used in node as like this .. can be iinly used in browser console .. because it is an document. .. we can use alert in node with different syntax;
+
+// We avoid semicolon in react and node.
---
+++ WEB DEVELOPMENT/JavaScript/test.js
@@ -0,0 +1,8 @@
+console.log("Hello"); // semocolon may or may not be written;
+const num = 1445;
+let a = "This is a";
+var b = "Email"; // Prefer to not to use var , because of issue in block scope  and functional scope   --> still used in old js
+c = "Why this is printing"; //  not a good way , but works
+let d; //  --> Assign to Undefined
+console.table([num, a, b, c, d]); // Prints all in a table format
+
