46ce198944783b282a76f79295387120053b68e5
Adhishthan <adhishthanashok@gmail.com>
2024-05-10 22:52:51 +0530

    Understanding Node Project Setup

--- WEB DEVELOPMENT/Node.js/MainProject/src/index.js
+++ WEB DEVELOPMENT/Node.js/MainProject/src/index.js
@@ -1,12 +1,12 @@
 // require("dotenv").config({ path: ".env" });
 import dotenv from "dotenv";
-
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-  path: ".env",
+  path: "./.env",
 });
 connectDB()
   .then(() => {
--- WEB DEVELOPMENT/Node.js/MainProject/src/middlewares/multer.middleware.js
+++ WEB DEVELOPMENT/Node.js/MainProject/src/middlewares/multer.middleware.js
@@ -5,10 +5,11 @@ const storage = multer.diskStorage({
     cb(null, "./public/temp");
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname);
+    cb(null, file.originalname); // originalname option is not that good , because it uploaded the file with the same name as the user gives
   },
 });
 
 export const upload = multer({
   storage,
 });
+ 
