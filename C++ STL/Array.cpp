41ae7688440c6a5aae75d2de47a4dd2e4f75dc2f
Adhishthan Ashok <AdhishthanAshok>
2025-02-08 08:07:44 +0530

    Started Java Learning

--- C++ STL/Array.cpp
+++ C++ STL/Array.cpp
@@ -1,15 +1,17 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int size ;
-     cout << "Enter the size of the array : ";
-    cin >> size; 
-    array <int,10> arr; 
+int main()
+{
+    int size;
+    cout << "Enter the size of the array : ";
+    cin >> size;
+    vector<int> arr(size, 1213);
 
-    for(int count : arr ){
-        cout << count << "  " ;
+    for (int count : arr)
+    {
+        cout << count << "  ";
     }
-    
+
     return 0;
 }
